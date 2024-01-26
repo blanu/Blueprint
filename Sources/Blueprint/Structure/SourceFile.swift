8a1a51c7bd9cc4b7aa413ca96d1124d0793ef5a9
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-01-25 22:53:24 -0600

    optional header and imports

--- Sources/Blueprint/Structure/SourceFile.swift
+++ Sources/Blueprint/Structure/SourceFile.swift
@@ -11,11 +11,20 @@ import Text
 
 public struct SourceFile
 {
-    public let header: FileHeader
-    public let imports: ImportSection
+    public let header: FileHeader?
+    public let imports: ImportSection?
     public let structs: [Structure]
     public let enums: [Enumeration]
     public let functions: [Function]
+
+    public init(header: FileHeader? = nil, imports: ImportSection? = nil, structs: [Structure] = [], enums: [Enumeration] = [], functions: [Function] = [])
+    {
+        self.header = header
+        self.imports = imports
+        self.structs = structs
+        self.enums = enums
+        self.functions = functions
+    }
 }
 
 extension SourceFile: Blueprint
--- Sources/Blueprint/Swift/SwiftSourceFile.swift
+++ Sources/Blueprint/Swift/SwiftSourceFile.swift
@@ -15,12 +15,62 @@ extension SourceFile
     {
         let i = indent(indentation)
 
+        let headerText: Text
+        if let header = self.header
+        {
+            headerText = try header.transpile(.swift, indentation: indentation)
+        }
+        else
+        {
+            headerText = ""
+        }
+
+        let importText: Text
+        if let imports = self.imports
+        {
+            importText = try imports.transpile(.swift, indentation: indentation)
+        }
+        else
+        {
+            importText = ""
+        }
+
+        let structText: Text
+        if self.structs.isEmpty
+        {
+            structText = ""
+        }
+        else
+        {
+            structText = Text.join(try self.structs.map { try $0.transpile(.swift, indentation: indentation) }, "\n")
+        }
+
+        let enumText: Text
+        if self.enums.isEmpty
+        {
+            enumText = ""
+        }
+        else
+        {
+            enumText = Text.join(try self.enums.map { try $0.transpile(.swift, indentation: indentation) }, "\n")
+        }
+
+        let functionText: Text
+        if self.functions.isEmpty
+        {
+            functionText = ""
+        }
+        else
+        {
+            functionText = Text.join(try self.functions.map { try $0.transpile(.swift, indentation: indentation) }, "\n")
+        }
+
         return """
-        \(i)\(try self.header.transpile(.swift, indentation: indentation))
-        \(i)\(try self.imports.transpile(.swift, indentation: indentation))
-        \(i)\(Text.join(try self.structs.map { try $0.transpile(.swift, indentation: indentation) }, "\n"))
-        \(i)\(Text.join(try self.enums.map { try $0.transpile(.swift, indentation: indentation) }, "\n"))
-        \(i)\(Text.join(try self.functions.map { try $0.transpile(.swift, indentation: indentation) }, "\n"))
+        \(i)\(headerText)
+        \(i)\(importText)
+        \(i)\(structText)
+        \(i)\(enumText)
+        \(i)\(functionText)
         """.text
     }
 }
