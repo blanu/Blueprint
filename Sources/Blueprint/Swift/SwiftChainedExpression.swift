3c21dfae84fbcefec8ec7c3612fe5af17342f269
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-03-18 18:58:04 -0500

    trying to fix indentation

--- Sources/Blueprint/Swift/SwiftChainedExpression.swift
+++ Sources/Blueprint/Swift/SwiftChainedExpression.swift
@@ -13,10 +13,8 @@ extension ChainedExpression
 {
     func transpileSwift(_ indentation: Int) throws -> Text
     {
-        let i = indent(indentation)
-
         return """
-        \(i)\(try self.lvalue.transpile(.swift)).\(try self.rvalue.transpile(.swift, indentation: indentation))
+        \(try self.lvalue.transpile(.swift)).\(try self.rvalue.transpile(.swift, indentation: indentation))
         """.text
     }
 }
