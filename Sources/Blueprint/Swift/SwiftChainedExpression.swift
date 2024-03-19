cf65833d4a25f38a4c93d037848aef961f1174a7
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-03-19 15:27:34 -0500

    Move try and await to left side in chained expressions

--- Sources/Blueprint/Swift/SwiftChainedExpression.swift
+++ Sources/Blueprint/Swift/SwiftChainedExpression.swift
@@ -13,8 +13,43 @@ extension ChainedExpression
 {
     func transpileSwift(_ indentation: Int) throws -> Text
     {
-        return """
-        \(try self.lvalue.transpile(.swift)).\(try self.rvalue.transpile(.swift, indentation: indentation))
-        """.text
+        var suppressTryAwait: Bool = false
+
+        let tryingText: Text
+        if self.rvalue.trying
+        {
+            tryingText = "try "
+            suppressTryAwait = true
+        }
+        else
+        {
+            tryingText = ""
+        }
+
+        let asyncText: Text
+        if self.rvalue.async
+        {
+            asyncText = "try "
+            suppressTryAwait = true
+        }
+        else
+        {
+            asyncText = ""
+        }
+
+        if suppressTryAwait
+        {
+            let suppressedFunction = FunctionCall(trying: false, async: false, name: self.rvalue.name, genericTypeParameters: self.rvalue.genericTypeParameters, arguments: self.rvalue.arguments)
+
+            return """
+            \(tryingText)\(asyncText)\(try self.lvalue.transpile(.swift)).\(try suppressedFunction.transpile(Target.swift, indentation: indentation))
+            """.text
+        }
+        else
+        {
+            return """
+            \(tryingText)\(asyncText)\(try self.lvalue.transpile(.swift)).\(try self.rvalue.transpile(.swift, indentation: indentation))
+            """.text
+        }
     }
 }
